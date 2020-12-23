b395209790fc1a83c25cdb23dc053502dec284e9
qusehdgns <63845862+qusehdgns@users.noreply.github.com>
2020-12-23 20:16:34 +0900

    dong
    모델 구성중

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from adminapp.models import User
 
-# Register your models here.
+# 출력할 ResourceAdmin 클래스를 만든다
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'user_pw')
+
+# 클래스를 어드민 사이트에 등록한다.
+admin.site.register(User, UserAdmin)
--- adminapp/models.py
+++ adminapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# 사용자 데이터베이스
+class User(models.Model):
+    # 사용자 ID (PK)
+    user_id = models.CharField(max_length=50, primary_key=True)
+    
+    # 사용자 PW
+    user_pw = models.CharField(max_length=50)
+
+    class Meta:
+        # 데이터베이스 테이블 명 'users'
+        db_table = 'users'
+
+class Requests(models.Model):
+    # 요청사항 기본키
+    r_num = models.AutoField(primary_key=True)
